e88f95df6e66fbe655a08006e3e018cdc289eaae
cybersport <bzuev662@gmail.com>
2024-10-11 12:22:06 +0300

    last commit

---
+++ app/src/main/java/com/example/kizhapkinzadanie5/MainActivity5.java
@@ -0,0 +1,32 @@
+package com.example.kizhapkinzadanie5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main5);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void Navigate5(View v5){
+        Toast.makeText(MainActivity5.this, "Настройки успешно сохранены", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity5.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
